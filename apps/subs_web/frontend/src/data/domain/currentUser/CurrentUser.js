8df3bc62a1ab97391b4a96e3599bdf0e4a97adcf
joaquimadraz <joaquim.adraz@gmail.com>
2017-11-18 18:07:39 +0000

    Separate between remoteData and local data

--- apps/subs_web/frontend/src/data/domain/currentUser/CurrentUser.js
+++ apps/subs_web/frontend/src/data/domain/currentUser/CurrentUser.js
@@ -1,21 +1,24 @@
 import { Record } from 'immutable'
 
-const currentUserObject = {
+const remoteData = {
   name: null,
   email: null,
+}
+
+const objectData = {
   authToken: null,
   isLogged: false,
   wasRequested: false,
 }
 
-const CurrentUserRecord = Record(currentUserObject)
+const CurrentUserRecord = Record(Object.assign({}, remoteData, objectData))
 
 class CurrentUser extends CurrentUserRecord {
   // Extend immutable js Record
 }
 
-export function parseCurrentUser(object) {
-  return new CurrentUser(object)
+export function parseCurrentUser(remoteData) {
+  return new CurrentUser(remoteData)
 }
 
 export default CurrentUser
