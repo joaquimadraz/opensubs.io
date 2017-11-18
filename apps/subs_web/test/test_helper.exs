d50d249e41aeb899e61b6ff43d1f42a8859b5266
joaquimadraz <joaquim.adraz@gmail.com>
2017-11-18 19:11:02 +0000

    Increase wallaby max wait time

--- apps/subs_web/test/test_helper.exs
+++ apps/subs_web/test/test_helper.exs
@@ -5,3 +5,4 @@ Ecto.Adapters.SQL.Sandbox.mode(Subs.Repo, :manual)
 System.cmd("sh", ["priv/build_frontend.sh"])
 
 Application.put_env(:wallaby, :base_url, SubsWeb.Endpoint.url)
+Application.get_env(:wallaby, :max_wait_time, 5_000)
