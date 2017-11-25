376bda91cc15355a3911d09b82c53865bbe28232
joaquimadraz <joaquim.adraz@gmail.com>
2017-11-25 16:58:55 +0000

    Build frontend when running test

--- apps/subs_web/test/test_helper.exs
+++ apps/subs_web/test/test_helper.exs
@@ -2,7 +2,7 @@ ExUnit.start()
 
 Ecto.Adapters.SQL.Sandbox.mode(Subs.Repo, :manual)
 
-# System.cmd("sh", ["priv/build_frontend.sh"])
+System.cmd("sh", ["priv/build_frontend.sh"])
 
 Application.put_env(:wallaby, :base_url, SubsWeb.Endpoint.url)
 Application.put_env(:wallaby, :chrome, [
