f627a9f728a921f274239e1c1338ddbfb16bff8e
joaquimadraz <joaquim.adraz@gmail.com>
2017-12-05 21:34:39 +0000

    Prevent link click on archive

--- apps/subs_web/frontend/src/components/SubscriptionListItem/SubscriptionListItem.jsx
+++ apps/subs_web/frontend/src/components/SubscriptionListItem/SubscriptionListItem.jsx
@@ -7,42 +7,50 @@ import Subscription from 'data/domain/subscriptions/Subscription'
 import Button from 'components/Button'
 import Styles from './Styles'
 
-const SubscriptionListItem = ({ subscription, onArchiveClick }) => (
-  <Styles
-    className="SubscriptionListItem br2 mb2 list"
-    background={subscription.color}
-    textColor={subscription.textColor}
-  >
-    <Link
-      to={routes.subscriptionsShow(subscription.id)}
-      className="flex items-center dim pointer no-underline"
+const SubscriptionListItem = ({ subscription, onArchiveClick }) => {
+  const handleArchiveClick = (event) => {
+    event.preventDefault()
+
+    onArchiveClick(subscription.id)
+  }
+
+  return (
+    <Styles
+      className="SubscriptionListItem br2 mb2 list"
+      background={subscription.color}
+      textColor={subscription.textColor}
     >
-      <div className="w-30 w-40-ns pa2 pa3-ns">
-        <span className="SubscriptionListItem--name">
-          {subscription.name}
-        </span>
-      </div>
-      <div className="w-30 w-20-ns pa2 pa3-ns tc">
-        <span className="SubscriptionListItem--amount">
-          {subscription.amount_currency_symbol}{subscription.amount}
-        </span>
-      </div>
-      <div className="w-30 w-20-ns pa2 pa3-ns tc">
-        <span className="SubscriptionListItem--next-bill-date">
-          {subscription.humanNextBillDate}
-        </span>
-      </div>
-      <div className="w-10 w-20-ns pa2 pa3-ns tr">
-        <Button
-          className="SubscriptionListItem--archive-button"
-          onClick={() => onArchiveClick(subscription.id)}
-        >
-          Archive
-        </Button>
-      </div>
-    </Link>
-  </Styles>
-)
+      <Link
+        to={routes.subscriptionsShow(subscription.id)}
+        className="flex items-center dim pointer no-underline"
+      >
+        <div className="w-30 w-40-ns pa2 pa3-ns">
+          <span className="SubscriptionListItem--name">
+            {subscription.name}
+          </span>
+        </div>
+        <div className="w-30 w-20-ns pa2 pa3-ns tc">
+          <span className="SubscriptionListItem--amount">
+            {subscription.amount_currency_symbol}{subscription.amount}
+          </span>
+        </div>
+        <div className="w-30 w-20-ns pa2 pa3-ns tc">
+          <span className="SubscriptionListItem--next-bill-date">
+            {subscription.humanNextBillDate}
+          </span>
+        </div>
+        <div className="w-10 w-20-ns pa2 pa3-ns tr">
+          <Button
+            className="SubscriptionListItem--archive-button"
+            onClick={handleArchiveClick}
+          >
+            Archive
+          </Button>
+        </div>
+      </Link>
+    </Styles>
+  )
+}
 
 SubscriptionListItem.propTypes = {
   subscription: PropTypes.instanceOf(Subscription).isRequired,
