e434abb0a812e0843d032bf9dd17fb8dea151a0f
joaquimadraz <joaquim.adraz@gmail.com>
2017-12-30 19:57:48 +0000

    Add subscriptions list component

--- apps/subs_web/frontend/src/routes/Subscriptions/Subscriptions.jsx
+++ apps/subs_web/frontend/src/routes/Subscriptions/Subscriptions.jsx
@@ -1,21 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { OrderedSet } from 'immutable'
+import { OrderedSet, Map } from 'immutable'
 
 import RemoteCall from 'data/domain/RemoteCall'
-import SubscriptionListItem from 'components/SubscriptionListItem'
+import SubscriptionsList from 'components/SubscriptionsList'
 
 const Subscriptions = ({ subscriptions, avgs, remoteCall }) => {
   if (remoteCall.loading) {
     return (<p>Loading...</p>)
   }
-
-  const renderSubscription = subscription => (
-    <SubscriptionListItem
-      key={subscription.id}
-      subscription={subscription}
-    />
-  )
 
   return (
     <div>
@@ -25,22 +18,14 @@ const Subscriptions = ({ subscriptions, avgs, remoteCall }) => {
         <span className="black-70 f2 b dib ml4">{avgs.get('yearly')} <small className="light-silver f5">per year</small></span>
       </div>
       <div className="mv4 bb bw2 b--near-white" />
-      <h3 className="black-70 f5 mb2 mt3">Payments</h3>
-      <ul className="pl0 mt0">
-        <li className="flex justify-around items-center lh-copy ph0-l light-silver">
-          <div className="w-40 pa2 f6 b ">Name</div>
-          <div className="w-30 pa2 f6 b tc">Next bill date</div>
-          <div className="w-20 pa2 f6 b tc">Alerts</div>
-          <div className="w-10 pa2 f6 b tr">Amount</div>
-        </li>
-        {subscriptions.map(renderSubscription)}
-      </ul>
+      <SubscriptionsList subscriptions={subscriptions} />
     </div>
   )
 }
 
 Subscriptions.propTypes = {
   subscriptions: PropTypes.instanceOf(OrderedSet).isRequired,
+  avgs: PropTypes.instanceOf(Map).isRequired,
   remoteCall: PropTypes.instanceOf(RemoteCall).isRequired,
 }
 
