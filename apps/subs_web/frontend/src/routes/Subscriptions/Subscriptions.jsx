e06505b997d88c53aa7a3d6a61a63d4cf0e4c05f
joaquimadraz <joaquim.adraz@gmail.com>
2017-12-31 17:13:32 +0000

    Remove avgs from all subscriptions list

--- apps/subs_web/frontend/src/routes/Subscriptions/Subscriptions.jsx
+++ apps/subs_web/frontend/src/routes/Subscriptions/Subscriptions.jsx
@@ -1,31 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { OrderedSet, Map } from 'immutable'
+import { OrderedSet } from 'immutable'
 
 import RemoteCall from 'data/domain/RemoteCall'
 import SubscriptionsList from 'components/SubscriptionsList'
 
-const Subscriptions = ({ subscriptions, avgs, remoteCall }) => {
+const Subscriptions = ({ subscriptions, remoteCall }) => {
   if (remoteCall.loading) {
     return (<p>Loading...</p>)
   }
 
   return (
-    <div>
-      <h3 className="black-70 f5">Average Expenses</h3>
-      <div>
-        <span className="black-70 f2 b dib">{avgs.get('monthly')} <small className="light-silver f5">per month</small></span>
-        <span className="black-70 f2 b dib ml4">{avgs.get('yearly')} <small className="light-silver f5">per year</small></span>
-      </div>
-      <div className="mv4 bb bw2 b--near-white" />
-      <SubscriptionsList subscriptions={subscriptions} />
-    </div>
+    <SubscriptionsList subscriptions={subscriptions} />
   )
 }
 
 Subscriptions.propTypes = {
   subscriptions: PropTypes.instanceOf(OrderedSet).isRequired,
-  avgs: PropTypes.instanceOf(Map).isRequired,
   remoteCall: PropTypes.instanceOf(RemoteCall).isRequired,
 }
 
