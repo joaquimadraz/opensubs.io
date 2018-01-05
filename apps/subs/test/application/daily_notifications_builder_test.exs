a464e618e67cdce92818465b596324509a8c444e
joaquimadraz <joaquim.adraz@gmail.com>
2018-01-05 17:04:19 +0000

    Move DTMock definition to setup

--- apps/subs/test/application/daily_notifications_builder_test.exs
+++ apps/subs/test/application/daily_notifications_builder_test.exs
@@ -4,16 +4,21 @@ defmodule Subs.Test.Application.DailyNotificationsBuilderTest do
   import Subs.Test.Support.Factory
   alias Subs.Application.DailyNotificationsBuilder
 
-  test "creates sub notification for specific day" do
+  setup do
+    now = ~N[2018-01-01T00:00:00Z]
+
     Test.Subs.DTMock
-    |> expect(:now, 2, fn -> ~N[2018-01-01T00:00:00Z] end)
-    |> expect(:beginning_of_day, fn _ -> ~N[2018-01-01T00:00:00Z] end)
+    |> expect(:now, 2, fn -> now end)
+    |> expect(:beginning_of_day, fn _ -> now end)
     |> expect(:end_of_day, fn _ -> ~N[2018-01-01T23:59:59Z] end)
 
+    [now: now]
+  end
+  test "creates sub notification for specific day", %{now: now} do
     user = insert(:user)
 
     subscription =
-      insert(:complete_subscription, user: user, next_bill_date: ~N[2018-01-01T00:00:00Z])
+      insert(:complete_subscription, user: user, next_bill_date: now)
 
     insert(:complete_subscription, user: user, next_bill_date: ~N[2018-01-02T00:00:00Z])
 
@@ -22,13 +27,13 @@ defmodule Subs.Test.Application.DailyNotificationsBuilderTest do
     # Subs.SubsNotification was created for a particular user
     assert subs_notification.user_id == user.id
     assert subs_notification.type == :daily
-    assert subs_notification.deliver_at == ~N[2018-01-01T00:00:00Z]
+    assert subs_notification.deliver_at == now
 
     # Notifier.Notification email was created for the user
     notification = subs_notification.notification
     assert notification.to == user.email
     assert notification.status == :pending
-    assert notification.notify_at == ~N[2018-01-01T00:00:00Z]
+    assert notification.notify_at == now
     assert notification.title == "You have 1 payment due today"
     assert notification.body == "Hello!\n\nDue today:\nCustom Service - GBP7.00 \n\n\nThanks!\n"
 
@@ -37,14 +42,7 @@ defmodule Subs.Test.Application.DailyNotificationsBuilderTest do
     assert subscription.id == assigned_subscription.id
   end
 
-  test "creates single notification with multiple subscriptions" do
-    now = ~N[2018-01-01T00:00:00Z]
-
-    Test.Subs.DTMock
-    |> expect(:now, 2, fn -> now end)
-    |> expect(:beginning_of_day, fn _ -> now end)
-    |> expect(:end_of_day, fn _ -> ~N[2018-01-01T23:59:59Z] end)
-
+  test "creates single notifier.notification with multiple subscriptions", %{now: now} do
     user = insert(:user)
 
     insert(:complete_subscription, name: "A", user: user, next_bill_date: now)
