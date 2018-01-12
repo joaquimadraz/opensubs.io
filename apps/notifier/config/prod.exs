34a75d2a038206eeebfa7ab1cd5d97d3d275a719
joaquimadraz <joaquim.adraz@gmail.com>
2018-01-12 09:16:54 +0000

    Fix crontab format to 1am
    Was set to every 1st of each month :facepalm:

--- apps/notifier/config/prod.exs
+++ apps/notifier/config/prod.exs
@@ -3,7 +3,7 @@ use Mix.Config
 config :notifier, Notifier.Scheduler,
   jobs: [
     # Every day at 1am
-    {"0 0 1 * *", {Notifier, :deliver_notifications, []}},
+    {"0 1 * * *", {Notifier, :deliver_notifications, []}},
   ]
 
 config :notifier, Notifier.Mailer,
