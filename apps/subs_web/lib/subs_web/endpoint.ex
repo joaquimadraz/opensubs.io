324f660b443d6bc61ed86c8eb4415479b590c80a
joaquimadraz <joaquim.adraz@gmail.com>
2018-03-04 10:55:12 +0000

    Set proper defaults for cookie session, eventhough are not being used

--- apps/subs_web/lib/subs_web/endpoint.ex
+++ apps/subs_web/lib/subs_web/endpoint.ex
@@ -39,8 +39,11 @@ defmodule SubsWeb.Endpoint do
   # Set :encryption_salt if you would also like to encrypt it.
   plug Plug.Session,
     store: :cookie,
-    key: "_subs_web_key",
-    signing_salt: "gHcD7ZWY"
+    secure: true,
+    encrypt: true,
+    key: Application.get_env(:subs_web, SubsWeb.Endpoint)[:session_cookie_name] || "_subs_web_key",
+    signing_salt: Application.get_env(:subs_web, SubsWeb.Endpoint)[:session_cookie_signing_salt] || "gHcD7ZWY",
+    encryption_salt: Application.get_env(:subs_web, SubsWeb.Endpoint)[:session_cookie_encryption_salt]
 
   use Appsignal.Phoenix
   plug SubsWeb.Router
